a389b47e5efacfc08b779d1d0aa891ddfb3284a6
Alexander <wolfking05101980@gmail.com>
2025-12-01 00:30:47 +0700

    feat(ads): Добавил функционал подробного просмотра объявления: - шаблон с деталями объявления - маршрут и обработчик этого маршрута

--- ads/urls.py
+++ ads/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('ads/', views.get_ads_list),
+    path('ads/<int:ad_id>', views.get_ad_details, name = 'ad_details'), 
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from ads.models import Advertisement
 
 
@@ -6,3 +6,9 @@ def get_ads_list(request):
   ads = Advertisement.objects.all()
   
   return render(request=request, template_name='ads/ad_list.html', context={'ads': ads})
+
+
+def get_ad_details(request, ad_id):
+  ad_for_view = get_object_or_404(Advertisement, id=ad_id)
+  
+  return render(request, 'ads/ad_details.html', {'ad': ad_for_view})
