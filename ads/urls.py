a3daa6023d75d0a409ac5dd9cb85f2db3bdfc77a
Alexander <wolfking05101980@gmail.com>
2025-12-01 16:55:10 +0700

    refactor(ads): Реализовал наследование шаблонов: - создал базовый шаблон base.html - наследовал от базового шаблона уже имеющиеся

--- ads/urls.py
+++ ads/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ads/', views.get_ads_list),
+    path('ads/', views.get_ads_list, name='ad_list'),
     path('ads/<int:ad_id>', views.get_ad_details, name = 'ad_details'), 
 ]
