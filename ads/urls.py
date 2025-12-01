8b482e6cc0dbf87da7db220fe2507af514f02306
Alexander <wolfking05101980@gmail.com>
2025-12-01 20:24:19 +0700

    feat(): Реализовал простой функционал добавления объявления

--- ads/urls.py
+++ ads/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('ads/', views.get_ads_list, name='ad_list'),
     path('ads/<int:ad_id>', views.get_ad_details, name = 'ad_details'), 
+    path('ads/add/', views.create_ad, name = 'ad_add')
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from ads.models import Advertisement
 
 
@@ -12,3 +12,22 @@ def get_ad_details(request, ad_id):
   ad_for_view = get_object_or_404(Advertisement, id=ad_id)
   
   return render(request, 'ads/ad_details.html', {'ad': ad_for_view})
+
+
+def create_ad(request):
+  if request.method == "GET":
+    return render(request, 'ads/ad_add.html')
+  
+  if request.method == "POST":
+    ad = Advertisement(
+      title = request.POST.get('title'),
+      text = request.POST.get('text'),
+      price = request.POST.get('price'),
+      contacts = request.POST.get('contacts')
+    )
+    if 'goods_image' in request.FILES:
+      ad.goods_image = request.FILES['goods_image']
+      
+    ad.save()
+      
+    return redirect('ad_details', ad_id = ad.id)
