e3035d3994561b202dc7357f8595f2ed0b43cc49
Alexander <wolfking05101980@gmail.com>
2026-02-15 19:12:59 +0700

    refactor(env): install python-dotenv: - create .env.example - update Django settings to use environment variables - update .gitignore to exclude .env

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l0m3=c&=piz1c8o4$r9i@2k%j!q$wnb3$4f42+(b-6&u9matm('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
