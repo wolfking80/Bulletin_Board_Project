a8d2bdc205a70e04ca55b696a733174575370bd7
Alexander <wolfking05101980@gmail.com>
2026-02-15 21:27:33 +0700

    feat(users/password): - add password reset functionality -> "Forgot your password?" - configure email (SMTP)

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,14 @@ AUTHENTICATION_BACKENDS = [
     # Модель пользователя для авторизации
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+# Настройки email для отправки писем
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Для тестирования в консоли
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -9,7 +11,38 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name="login"),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+# Можно так:
+# path(
+#   'password-change/',
+#   PasswordChangeView.as_view(
+#     template_name='users/pages/password_change.html',
+#     success_url=reverse_lazy('users:password_change_done')
+#   ),
+#   name='password_change'
+# ),
     path('password-change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+# Можно так:
+# path('password-change/done/', TemplateView.as_view(template_name='users/pages/password_change_done.html'), name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="users/pages/password_reset.html",
+        email_template_name='users/emails/password_reset.txt',
+        html_email_template_name='users/emails/password_reset.html',
+        subject_template_name='users/emails/subjects/password_reset.txt',
+        success_url=reverse_lazy("users:password_reset_instructions_sent")
+    ), name='password_reset'),
+
+    path('password-reset/instructions-sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="users/pages/password_reset_instructions_sent.html"
+    ), name='password_reset_instructions_sent'),
+
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="users/pages/password_reset_form.html",
+        success_url=reverse_lazy('users:password_reset_complete')
+    ), name='password_reset_set_new'),
+
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="users/pages/password_reset_complete.html"
+    ), name='password_reset_complete'),
     path("toggle-theme/", views.toggle_theme, name="toggle_theme"),
     path("<str:username>/", views.ProfileView.as_view(), name='profile'),
 ]
