003287e6485b5f8a13cb56eae99b7e055992e6d0
wolfking80 <wolfking05101980@gmail.com>
2026-05-20 09:16:22 +0700

    feat(users): add 'email_confirmed' and 'notifications_enabled' fields for editing in admin panel

--- users/admin.py
+++ users/admin.py
@@ -5,14 +5,22 @@ from users.models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
   # Что видим в таблице (списке)
-  list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'avatar')
+  list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'email_confirmed', 'notifications_enabled','avatar')
+  # Добавляем фильтры
+  list_filter = UserAdmin.list_filter + ('email_confirmed', 'notifications_enabled', 'selected_theme')
+  # Поля для поиска
+  search_fields = UserAdmin.search_fields + ('phone_number',)
   # Что заполняем при СОЗДАНИИ нового юзера
   add_fieldsets = UserAdmin.add_fieldsets + (
-    (None, {'fields': ('email', 'first_name', 'last_name', 'avatar')}),
+    (None, {'fields': ('email', 'first_name', 'last_name', 'email_confirmed', 'notifications_enabled', 'avatar')}),
   )
   # Поля при РЕДАКТИРОВАНИИ (внутри карточки)
   fieldsets = UserAdmin.fieldsets + (
         ('Дополнительная информация', {
             'fields': ('avatar',)
+        }),
+        ('Настройки уведомлений', {
+            'fields': ('email_confirmed', 'notifications_enabled'),
+            'description': 'Управление email уведомлениями и подтверждением'
         }),
     )
